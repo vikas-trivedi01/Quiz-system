133fa57a3aac9174b982b621cb87d5081988a7b2
vikas trivedi <vikastrivedi4522@gmail.com>
2025-06-09 14:57:22 +0530

    add different options related to quizzes as actions

--- frontend/src/components/ListItem.jsx
+++ frontend/src/components/ListItem.jsx
@@ -2,9 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTrash,
   faUpRightFromSquare,
-  faFilePen
+  faFilePen,
+  faSquareXmark,
+  faEllipsisVertical
 } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const ListItem = ({
   quizName,
@@ -36,29 +39,35 @@ const ListItem = ({
     fontWeight: "600",
     width: "200px",
   };
-
-  const editQuestionsButtonStyle = {
+  
+  const editButtonStyle = {
     backgroundColor: "var(--clr-primary)",
     color: "#fff",
     borderRadius: "var(--border-radius)",
-    padding: "5px 10px",
+    padding: "5px 15px",
     border: "none",
     cursor: "pointer",
-    gap: "22px",
     fontWeight: "600",
-    width: "100px",
+    width: "200px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: "10px",
   };
 
   const deleteQuizButtonStyle = {
     backgroundColor: "var(--clr-accent)",
-    color: "#000",
+    color: "#fff",
     borderRadius: "var(--border-radius)",
-    padding: "5px 10px",
+    padding: "5px 15px",
     border: "none",
     cursor: "pointer",
-    gap: "22px",
     fontWeight: "600",
-    width: "100px",
+    width: "200px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: "10px",
   };
 
   const labelStyle = {
@@ -82,6 +91,22 @@ const ListItem = ({
     margin: "10px",
   };
 
+  const actionsButtonStyle = {
+    backgroundColor: "#000",
+    color: "#fff",
+    borderRadius: "var(--border-radius)",
+    padding: "10px 20px",
+    border: "none",
+    cursor: "pointer",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "25px",
+    fontWeight: "600",
+    marginRight: "30px",
+    width: "150px",
+  };
+
   const editQuestions = () => {
     if (isAdmin) {
       navigate("/quizzes/edit/questions", {
@@ -103,6 +128,9 @@ const ListItem = ({
     });
   };
 
+  const [actionsOpen, setActionsOpen] = useState(false);
+
+  console.log(actionsOpen)
   return (
     <div
       style={{
@@ -115,98 +143,120 @@ const ListItem = ({
         boxShadow: "0 4px 12px rgba(0,0,0,0.05)",
       }}
     >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          flexWrap: "wrap",
-          alignItems: "center",
-          marginBottom: "20px",
-        }}
-      >
-        <div style={{ fontSize: "1.5rem", fontWeight: "700", color: "#333" }}>
-          {quizName}
-        </div>
-
-        {isAdmin ? (
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              gap: "10px",
-            }}
-          >
-            <button
-              style={editQuestionsButtonStyle}
-              title="Edit Question"
-              onClick={() => editQuestions()}
-            >
-              <FontAwesomeIcon icon={faFilePen} />
-            </button>
-            <button
-              style={deleteQuizButtonStyle}
-              title="Delete Quiz"
-              onClick={() => deleteQuiz()}
-            >
-              <FontAwesomeIcon icon={faTrash} />
-            </button>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+            alignItems: "center",
+            marginBottom: "20px",
+          }}
+        >
+          <div style={{ fontSize: "1.5rem", fontWeight: "700", color: "#333" }}>
+            {quizName}
           </div>
-        ) : (
-          <button style={joinButtonStyle}>
-            Join Quiz
-            <FontAwesomeIcon icon={faUpRightFromSquare} />
-          </button>
-        )}
-      </div>
 
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "16px",
-          justifyContent: "space-between",
-        }}
-      >
-        <div style={itemBox}>
-          <div style={labelStyle}>Category</div>
-          <div style={valueStyle}>{category}</div>
+          {isAdmin ? (
+            <div>
+              <button onClick={() => setActionsOpen(prev => !prev)} style={actionsButtonStyle}>
+                Actions{" "}
+              {actionsOpen ? (
+                <FontAwesomeIcon icon={faSquareXmark} className="ms-1" />
+              ) : (
+                <FontAwesomeIcon icon={faEllipsisVertical} className="ms-1" />
+              )}
+              </button>
+            </div>
+            ) :
+            (
+                <button style={joinButtonStyle}>
+              Join Quiz
+              <FontAwesomeIcon icon={faUpRightFromSquare} />
+            </button>
+            )
+               
+          }
+
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "16px",
+            justifyContent: "space-between",
+          }}
+        >
+          <div style={itemBox}>
+            <div style={labelStyle}>Category</div>
+            <div style={valueStyle}>{category}</div>
+          </div>
+
+          <div style={itemBox}>
+            <div style={labelStyle}>Questions</div>
+            <div style={valueStyle}>{numberOfQuestions}</div>
+          </div>
+
+          <div style={itemBox}>
+            <div style={labelStyle}>Participants</div>
+            <div style={valueStyle}>{participants ?? "N/A"}</div>
+          </div>
+
+          <div style={itemBox}>
+            <div style={labelStyle}>Each Question Marks</div>
+            <div style={valueStyle}>{eachQuestionMarks}</div>
+          </div>
+
+          <div style={itemBox}>
+            <div style={labelStyle}>Total Marks</div>
+            <div style={valueStyle}>{totalMarks}</div>
+          </div>
+
+          <div style={itemBox}>
+            <div style={labelStyle}>Difficulty</div>
+            <div style={valueStyle}>{difficulty}</div>
+          </div>
+
+          <div style={itemBox}>
+            <div style={labelStyle}>Status</div>
+            <div style={valueStyle}>{status}</div>
+          </div>
+
+          <div style={itemBox}>
+            <div style={labelStyle}>Created By</div>
+            <div style={valueStyle}>{createdBy} {isAdmin ? "(You)" : null}</div>
+          </div>
         </div>
 
-        <div style={itemBox}>
-          <div style={labelStyle}>Questions</div>
-          <div style={valueStyle}>{numberOfQuestions}</div>
-        </div>
-
-        <div style={itemBox}>
-          <div style={labelStyle}>Participants</div>
-          <div style={valueStyle}>{participants ?? "N/A"}</div>
-        </div>
-
-        <div style={itemBox}>
-          <div style={labelStyle}>Each Question Marks</div>
-          <div style={valueStyle}>{eachQuestionMarks}</div>
-        </div>
-
-        <div style={itemBox}>
-          <div style={labelStyle}>Total Marks</div>
-          <div style={valueStyle}>{totalMarks}</div>
-        </div>
-
-        <div style={itemBox}>
-          <div style={labelStyle}>Difficulty</div>
-          <div style={valueStyle}>{difficulty}</div>
-        </div>
-
-        <div style={itemBox}>
-          <div style={labelStyle}>Status</div>
-          <div style={valueStyle}>{status}</div>
-        </div>
-
-        <div style={itemBox}>
-          <div style={labelStyle}>Created By</div>
-          <div style={valueStyle}>{createdBy} {isAdmin ? "(You)" : null}</div>
-        </div>
+        <div
+              className={`px-3 py-4 mt-3 mb-0 gap-5 ms-5 d-${actionsOpen ? "flex justify-content-between" : "none"}`}
+              style={{ 
+                    borderRadius: "var(--border-radius)",
+                    }}
+            >
+                  <button
+                style={editButtonStyle}
+                title="Edit Questions"
+                onClick={() => editQuestions()}
+              >
+                Edit Questions{" "}
+                <FontAwesomeIcon icon={faFilePen} />
+              </button>
+              <button
+                style={editButtonStyle}
+                title="Edit Quiz Details"
+                onClick={() => deleteQuiz()}
+              >
+                Edit Quiz{" "}
+                <FontAwesomeIcon icon={faUpRightFromSquare} />
+              </button>
+              <button
+                style={deleteQuizButtonStyle}
+                title="Delete Quiz"
+                onClick={() => deleteQuiz()}
+              >
+                Delete Quiz{" "}
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+            </div>
       </div>
     </div>
   );

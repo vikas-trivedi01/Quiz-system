5482df5184739e8a3176db0783b0dcbfbbffaf43
vikas trivedi <vikastrivedi4522@gmail.com>
2025-06-27 11:18:03 +0530

    add counter-up for counts in home page

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,27 @@ import userRouter from "./routes/user.routes.js"
 import quizRouter from "./routes/quiz.routes.js"
 import authRouter from "./routes/auth.routes.js"
 
+import { asyncErrorHandler } from "./utils/asyncErrorHandler.js";
+import { User } from "./models/user.model.js";
+import { ApiResponse } from "./utils/ApiResponse.js";
+import { Quiz } from "./models/quiz.model.js";
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/quizzes", quizRouter);
 
+app.get("/api/v1/counts", asyncErrorHandler(async (req, res) => {
+    const users = await User.find({});
+    const quizzes = await Quiz.find({});
+    const categories = await Quiz.find().select("category");
+
+    const counts = [
+        Array.isArray(users) ? users.length : 0 ,
+        Array.isArray(quizzes) ? quizzes.length : 0 ,
+        Array.isArray(categories) ? categories.length : 0 ,
+    ];
+
+    res.status(200)
+        .json(new ApiResponse({counts}, "Counts fetched successfully", 200));
+}));
 export { app }
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,8 +7,31 @@ import userTwo from "../assets/users/user-two.png";
 import userThree from "../assets/users/user-three.png";
 
 import { NavLink } from "react-router-dom";
+import CountUp from "react-countup";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import { BACKEND_URL } from "../assets/constants.js";
 
 const Home = () => {
+  const [counts, setCounts] = useState([]);
+  useEffect(() => {
+    const getCounts = async () => {
+      try {
+        const response = await axios.get(`${BACKEND_URL}/counts`, {
+          withCredentials: true,
+        });
+
+        if (response?.status === 200) {
+          setCounts(response?.data?.data?.counts);
+        }
+      } catch (error) {
+        alert(error?.message);
+      }
+    };
+    getCounts();
+  }, []);
+
   return (
     <>
       <section className="bg-gradient text-white py-5">
@@ -23,14 +46,29 @@ const Home = () => {
           </div>
           <div className="col-md-6 text-dark">
             <h1 className="display-4 fw-bold mb-3">
-              Participate.<br /> Learn.<br /> Grow.
+              Participate.
+              <br /> Learn.
+              <br /> Grow.
             </h1>
             <p className="lead mb-4">
-              Enhance your understanding with engaging quizzes. <br /> Get immediate results and boost your growth.            
+              Enhance your understanding with engaging quizzes. <br /> 
+              Get immediate results and boost your growth.
             </p>
             <div className="d-flex gap-3">
-              <NavLink to="/quizzes/quizlist" className="btn btn-lg text-light" style={{ backgroundColor: "var(--clr-primary)"}}>Explore Quizzes</NavLink>
-              <NavLink to="/info/about" className="btn btn-outline-light btn-lg" style={{ backgroundColor: "var(--clr-accent)"}}>About Us</NavLink>
+              <NavLink
+                to="/quizzes/quizlist"
+                className="btn btn-lg text-light"
+                style={{ backgroundColor: "var(--clr-primary)" }}
+              >
+                Explore Quizzes
+              </NavLink>
+              <NavLink
+                to="/info/about"
+                className="btn btn-outline-light btn-lg"
+                style={{ backgroundColor: "var(--clr-accent)", width: "200px" }}
+              >
+                About Us
+              </NavLink>
             </div>
           </div>
         </div>
@@ -41,9 +79,18 @@ const Home = () => {
           <h2 className="section-title mb-5">Why Choose Us?</h2>
           <div className="row">
             {[
-              { title: "Wide Range of Topics", desc: "Explore quizzes on various categories to enhance your knowledge." },
-              { title: "Instant Feedback", desc: "Get immediate scoring and insights to improve your performance." },
-              { title: "User-Friendly Interface", desc: "Enjoy a clean, intuitive platform designed for everyone." }
+              {
+                title: "Wide Range of Topics",
+                desc: "Explore quizzes on various categories to enhance your knowledge.",
+              },
+              {
+                title: "Instant Feedback",
+                desc: "Get immediate scoring and insights to improve your performance.",
+              },
+              {
+                title: "User-Friendly Interface",
+                desc: "Enjoy a clean, intuitive platform designed for everyone.",
+              },
             ].map((feature, idx) => (
               <div className="col-md-4 mb-4" key={idx}>
                 <div className="feature-card p-4 border rounded shadow-sm h-100">
@@ -56,15 +103,55 @@ const Home = () => {
         </div>
       </section>
 
-      
       <section className="cta bg-dark text-white text-center py-5">
         <div className="container">
           <h2 className="mb-3 text-info">Your Learning Journey Starts Now</h2>
-          <p className="fs-5 mb-4">Join thousands of learners already mastering wide range of quizzes with us.</p>
-          <NavLink to="/users/signup" className="btn btn-lg px-5" style={{ backgroundColor: "var(--clr-primary)"}}>Get Started</NavLink>
+          <p className="fs-5 mb-4">
+            Join thousands of learners already mastering wide range of quizzes with us.
+          </p>
+          <NavLink
+            to="/users/signup"
+            className="btn btn-lg px-5"
+            style={{ backgroundColor: "var(--clr-primary)" }}
+          >
+            Get Started
+          </NavLink>
         </div>
       </section>
 
+      <section className="counter-section py-5 text-center bg-white">
+        <div className="container">
+          <h2 className="mb-5">Platform Achievements</h2>
+          <div className="row justify-content-center g-4">
+            {Array.isArray(counts) && counts.length > 0 ? (
+              counts.map((count, idx) => {
+                return (
+                  <div className="col-md-3" key={idx}>
+                    <h1
+                      className={`display-4 ${
+                        ["text-primary", "text-success", "text-warning"][idx]
+                      }`}
+                    >
+                      <CountUp end={count} duration={2} />+
+                    </h1>
+                    <p className="fs-5">
+                      {
+                        [
+                          "Registered Users",
+                          "Quizzes Available",
+                          "Categories Available",
+                        ][idx]
+                      }
+                    </p>
+                  </div>
+                );
+              })
+            ) : (
+              <p className="fs-3 fw-bold">Loading counts...</p>
+            )}
+          </div>
+        </div>
+      </section>
 
       <section className="testimonials py-5 bg-white">
         <div className="container text-center">
@@ -75,11 +162,13 @@ const Home = () => {
                 <Testimonial
                   userAvatar={img}
                   userName={["Rahul", "Abhay", "Parth"][idx]}
-                  feedback={[
-                    "I appreciate the diverse range of topics available. It allows me to explore new areas of interest and expand my knowledge!",
-                    "The user interface is incredibly intuitive. I can easily navigate through quizzes and track my progress without any hassle!",
-                    "This platform has completely transformed my approach to learning. The interactive features keep me engaged and motivated!"
-                  ][idx]}
+                  feedback={
+                    [
+                      "I appreciate the diverse range of topics available. It allows me to explore new areas of interest and expand my knowledge!",
+                      "The user interface is incredibly intuitive. I can easily navigate through quizzes and track my progress without any hassle!",
+                      "This platform has completely transformed my approach to learning. The interactive features keep me engaged and motivated!",
+                    ][idx]
+                  }
                   date={["23 May, 2024", "03 Jan, 2022", "13 Apr, 2024"][idx]}
                 />
               </div>
@@ -87,7 +176,6 @@ const Home = () => {
           </div>
         </div>
       </section>
-
     </>
   );
 };
